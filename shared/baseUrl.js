44433f152ae1974e3bef1ab1d4ab9c497c5ac956
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-07 00:20:34 -0500

    Starting JSON Server

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,4 @@
-export const baseURL = "http://192.168.1.3:3001/";
+export const baseUrl = "http://192.168.1.3:3001/";
+
+// json-server -H 192.168.1.3 --watch db.json -p 3001 -d 2000
+// 97.104.206.173
