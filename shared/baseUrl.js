b69cc28303f79a38f5ea73a599e50e20d7e3db5a
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-07 03:03:41 -0500

    Fetch Protocol Defined

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,4 +1,3 @@
 export const baseUrl = "http://192.168.1.3:3001/";
 
-// json-server -H 192.168.1.3 --watch db.json -p 3001 -d 2000
-// 97.104.206.173
+// json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
