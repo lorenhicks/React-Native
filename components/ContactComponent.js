312009e1227131c5ef054cf8d8d35897646005bf
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-14 22:18:15 -0500

    Animatable Embedded

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -4,6 +4,7 @@ import { Card } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
 import { PROMOTIONS } from "../shared/promotions";
 import { PARTNERS } from "../shared/partners";
+import * as Animatable from "react-native-animatable";
 
 class Contact extends Component {
   constructor(props) {
@@ -22,13 +23,15 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
-        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
-          <Text>1 Nucamp Way</Text>
-          <Text>Seattle, WA 98001</Text>
-          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-          <Text>Phone: 1-206-555-1234</Text>
-          <Text>Email: campsites@nucamp.co</Text>
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
