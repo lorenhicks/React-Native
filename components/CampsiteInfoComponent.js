dc3f17ceea903aa4cdb349813fc276634c6520a7
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-16 11:26:59 -0500

    Reservation Gesture State Added

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -38,6 +38,8 @@ function RenderCampsite(props) {
 
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
 
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => {
@@ -69,6 +71,8 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     },
