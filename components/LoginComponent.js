fef42c63cf06c54f55640972c75b221b0d73379e
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-01-23 00:53:32 -0500

    Button Package Conversion

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { baseUrl } from "../shared/baseUrl";
+import { LogBox } from "react-native";
 
 class LoginTab extends Component {
   constructor(props) {
@@ -120,6 +121,20 @@ class LoginTab extends Component {
 }
 
 class RegisterTab extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      remember: false,
+      imageUrl: baseUrl + "images/logo.png",
+    };
+  }
+
   static navigationOptions = {
     title: "Register",
     tabBarIcon: ({ tintColor }) => (
@@ -130,8 +145,93 @@ class RegisterTab extends Component {
       />
     ),
   };
+
+  handleRegister() {
+    console.log(JSON.stringify(this.state));
+    if (this.state.remember) {
+      SecureStore.setItemAsync(
+        "userinfo",
+        JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      SecureStore.deleteItemAsync("userinfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
+  }
+
   render() {
-    return <ScrollView></ScrollView>;
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Input
+            placeholder="Username"
+            leftIcon={{ type: "font-awesome", name: "user-o" }}
+            onChangeText={(username) => this.setState({ username })}
+            value={this.state.username}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder="Password"
+            leftIcon={{ type: "font-awesome", name: "key" }}
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder="First Name"
+            leftIcon={{ type: "font-awesome", name: "user-o" }}
+            onChangeText={(firstname) => this.setState({ firstname })}
+            value={this.state.firstname}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder="Last Name"
+            leftIcon={{ type: "font-awesome", name: "user-o" }}
+            onChangeText={(lastname) => this.setState({ lastname })}
+            value={this.state.lastname}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder="Email"
+            leftIcon={{ type: "font-awesome", name: "envelope-o" }}
+            onChangeText={(email) => this.setState({ email })}
+            value={this.state.email}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <CheckBox
+            title="Remember Me"
+            center
+            checked={this.state.remember}
+            onPress={() => this.setState({ remember: !this.state.remember })}
+            containerStyle={styles.formCheckbox}
+          />
+          <View style={styles.formButton}>
+            <Button
+              onPress={() => this.handleRegister()}
+              title="Register"
+              icon={
+                <Icon
+                  name="user-o"
+                  type="font-awesome"
+                  color="#fff"
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              buttonStyle={{ backgroundColor: "#5637DD" }}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
